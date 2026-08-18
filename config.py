7b35eb2b3a3a9f9eed69437204b75fc8771770a9
Harsha2626 <harsha.shivanand@midevops.io>
2026-08-18 15:00:40 +0530

    bug fixes

--- config.py
+++ config.py
@@ -9,6 +9,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'migrc-dev-secret-key-change-in-production')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'migrc.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_pre_ping': True,
+        'pool_recycle': 280,
+    }
     UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 10MB max file upload
     ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'csv', 'xlsx', 'doc', 'docx'}
